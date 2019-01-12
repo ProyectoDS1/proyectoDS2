4c5610fda65c6d840b2e384b2a50b8ac6f11c81e
FerSaul <fsmoreno@espol.edu.ec>
2019-01-12 16:25:30 -0500

    Proyecto de Diseño pasado a netbeans.

---
+++ ProyectoDS/src/Modelos/Comprador.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelos;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author User-PC
+ */
+public class Comprador extends Usuario{
+    
+    ArrayList<Pedido> misPedidos;
+    ArrayList<Calificacion> misCalificaciones;
+    
+    public Pedido comprar(Producto articulo){
+     
+    }
+    
+    public ArrayList<Pedido> mostrarPedidos(){
+      return misPedidos;
+    }
+    
+    public ArrayList<Pedido> mostrarHistorialPedidos(){
+      return misPedidos;
+    }
+    
+}
---
+++ ProyectoDS/src/Modelos/Usuario.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelos;
+
+/**
+ *
+ * @author User-PC
+ */
+public abstract class Usuario {
+    protected String email;
+    protected String contrasenia;
+    protected String nombre;
+    protected String apellido;
+    protected String telefono;
+    protected boolean whatsapp;
+    protected String direccion;
+    protected String matricula;
+    protected boolean activo;
+    private Usuario usuarioActual;
+    
+    public void iniciarSesion(){
+    }
+    
+    public void cerrarSesion(){
+    }
+    
+    public Usuario getUsuarioActual(){
+      return this.usuarioActual;
+    }
+    
+    public void setUsuarioActual(Usuario user){
+      this.usuarioActual = user;
+    }
+    
+}
